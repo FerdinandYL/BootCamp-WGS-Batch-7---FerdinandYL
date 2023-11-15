63b87db3888f31524c962dabd2e65124372c7dfa
FerdinandYL <yusakferdinand@gmail.com>
2023-11-15 16:21:29 +0700

    Mempelajari Middleware

--- Day7/app.js
+++ Day7/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+app.use(morgan('dev'))
+app.use(express.static("public"));
+
+app.use((req,res,next)=>{
+    console.log('Time : ', Date.now());
+    next();
+})
+
 app.get('/', (req,res)=>{
     contact = [
         {
