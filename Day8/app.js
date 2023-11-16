96109f65ec0bd32c51dba95f02a3f6f1b5ce6c20
FerdinandYL <yusakferdinand@gmail.com>
2023-11-16 15:34:28 +0700

    Add CRUD

--- Day8/app.js
+++ Day8/app.js
@@ -20,18 +20,33 @@ app.post('/contact', (req,res)=>{
     let contact = {'nama':req.body.nama, 'telepon':req.body.telepon, 'email':req.body.email};
     services.checkFile();
     let contacts = services.getContacts();
-    contacts.push(contact);
-    services.setContacts(contacts);
-    services.saveContact();
-    res.render('contact', {title:'Contacts', contacts:services.getContacts()});
+    if(req.body.method == 'update'){
+        contacts.splice(req.body.id, 1, contact);
+        services.setContacts(contacts);
+        services.saveContact();
+        res.redirect('contact', {title:"Contact", contacts:services.getContacts()})
+    } else {
+        contacts.push(contact);
+        services.setContacts(contacts);
+        services.saveContact();
+        res.redirect('contact');
+    }
 })
 app.get('/contact', (req,res)=>{
     services.checkFile();
     let contacts = services.getContacts();
     if(req.query.id != null){
-        res.render('detail', {title:"Detail Contact", detail:contacts[req.query.id]})
+        res.render('detail', {title:"Detail Contact", contact:contacts[req.query.id]})
+    }else if(req.query.update != null){
+        res.render('update', {title:"Update Contact", contact:contacts[req.query.update], id:req.query.update})
+    }else if(req.query.delete != null){
+        contacts.splice(req.query.delete, 1);
+        services.setContacts(contacts);
+        services.saveContact();
+        res.render('contact', {title:"Deleted Contact", contacts:contacts})
+    }else {
+        res.render('contact',{title:"Contacts", contacts:contacts});
     }
-    res.render('contact',{title:"Contacts", contacts:contacts});
 })
 app.get('/about', (req,res)=>{
     res.render('about', {title:"About Page"})
