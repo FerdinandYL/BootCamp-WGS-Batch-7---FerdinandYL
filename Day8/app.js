95b82cfc58e6a6d5c6c89769a49f33acb8bd7dbe
FerdinandYL <yusakferdinand@gmail.com>
2023-11-16 17:00:48 +0700

    Memperbaiki CRUD.

--- Day8/app.js
+++ Day8/app.js
@@ -2,12 +2,14 @@ const services = require('./services/services');
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const { body, validationResult } = require('express-validator');
 const morgan = require('morgan');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(morgan('dev'))
@@ -16,22 +18,34 @@ app.use(express.static("public"));
 app.get('/', (req,res)=>{
     res.render('home', {title:"home",nama:"Ferdinand"})
 })
-app.post('/contact', (req,res)=>{
-    let contact = {'nama':req.body.nama, 'telepon':req.body.telepon, 'email':req.body.email};
-    services.checkFile();
-    let contacts = services.getContacts();
-    if(req.body.method == 'update'){
-        contacts.splice(req.body.id, 1, contact);
-        services.setContacts(contacts);
-        services.saveContact();
-        res.redirect('contact', {title:"Contact", contacts:services.getContacts()})
-    } else {
-        contacts.push(contact);
-        services.setContacts(contacts);
-        services.saveContact();
-        res.redirect('contact');
+app.post('/contact', 
+    [
+        body('telepon').isMobilePhone('id-ID').withMessage('Nomor HP yang anda masukan salah!'),
+        body('email').isEmail().withMessage('Email yang anda masukan salah!')
+    ],
+    (req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        } else {
+            let contact = {'nama':req.body.nama, 'telepon':req.body.telepon, 'email':req.body.email};
+            services.checkFile();
+            let contacts = services.getContacts();
+            if(req.body.method == 'update'){
+                contacts.splice(req.body.id, 1, contact);
+                services.setContacts(contacts);
+                services.saveContact();
+                res.redirect('contact')
+            } else {
+                contacts.push(contact);
+                services.setContacts(contacts);
+                services.saveContact();
+                res.redirect('contact');
+            }
+        }
     }
-})
+)
+
 app.get('/contact', (req,res)=>{
     services.checkFile();
     let contacts = services.getContacts();
