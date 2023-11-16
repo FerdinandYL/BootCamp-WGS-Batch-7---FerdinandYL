7c1c5e41208a90a06ee27096559f5aea473114f2
FerdinandYL <yusakferdinand@gmail.com>
2023-11-16 14:24:29 +0700

    Add detail using new page method.

---
+++ Day8/app.js
@@ -0,0 +1,42 @@
+const services = require('./services/services');
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(morgan('dev'))
+app.use(express.static("public"));
+
+app.get('/', (req,res)=>{
+    res.render('home', {title:"home",nama:"Ferdinand"})
+})
+app.post('/contact', (req,res)=>{
+    let contact = {'nama':req.body.nama, 'telepon':req.body.telepon, 'email':req.body.email};
+    services.checkFile();
+    let contacts = services.getContacts();
+    contacts.push(contact);
+    services.setContacts(contacts);
+    services.saveContact();
+    res.render('contact', {title:'Contacts', contacts:services.getContacts()});
+})
+app.get('/contact', (req,res)=>{
+    services.checkFile();
+    let contacts = services.getContacts();
+    if(req.query.id != null){
+        res.render('detail', {title:"Detail Contact", detail:contacts[req.query.id]})
+    }
+    res.render('contact',{title:"Contacts", contacts:contacts});
+})
+app.get('/about', (req,res)=>{
+    res.render('about', {title:"About Page"})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Example app listening on port : ${PORT}`);
+})
