e27180c97d6b33fc4b22c7d2f8d2fca67990bf35
FerdinandYL <yusakferdinand@gmail.com>
2023-11-23 13:46:26 +0700

    Add like button using state.

--- Day13/contacts_app/src/App.js
+++ Day13/contacts_app/src/App.js
@@ -1,50 +1,108 @@
-const { faker } = require('@faker-js/faker');
+import React from 'react';
+import { faker } from '@faker-js/faker';
 
-export default function App(){
-  
-  const USERS = faker.helpers.multiple(createRandomUser, {count: 10,});
-  const comments = USERS.map((user)=>{
-    return <Comment nama={user.username} img={user.avatar} registeredAt={user.registeredAt} komentar={user.comment}/>
-  });
-
-  return comments;
+class Comments extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return this.props.data.map((dataComment, index) => (
+        <div className='commentContainer' key={index}>
+          <CommentContainer
+            avatar = {dataComment.avatar}
+            name = {dataComment.name}
+            time = {dataComment.time}
+            comment = {dataComment.comment}
+            like = {dataComment.like}
+            />
+        </div>
+      )
+    )
+  }
 }
 
-// function Person({nama, pekerjaan}){
-//   return(
-//     <>
-//       <h1> Hi nama saya {nama},</h1>
-//       <h2> dan pekerjaan saya adalah {pekerjaan}</h2>
-//     </>
-//   );
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     like: this.props.like || 0,
+//   };
 // }
+class CommentContainer extends React.Component {
 
-function Comment({nama, img, registeredAt, komentar}){
-  return (
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={img}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            {nama}
+  state = {
+    like: this.props.like
+  }
+
+  handleLikeClick = () => {
+    this.setState((prevState) => ({ like: prevState.like + 1 }));
+  }
+
+  render(){
+    return (
+      <div className="ui container comments">
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img alt="avatar" src={this.props.avatar} />
           </a>
-          <div className="metadata">
-            <span className="date">{registeredAt}</span> 
+          <div className="content">
+            <a href="/" className="author">
+              {this.props.name}
+            </a>
+            <div className="metadata">
+              <span className="date">{this.props.time}</span>
+              <div className="rating">
+                <i className="heart icon"></i>
+                Liked:{this.state.like}
+              </div>
+            </div>
+            <div className="text">{this.props.comment}</div>
+            <div className="actions">
+              <button className="likes" onClick={this.handleLikeClick}>Like</button>
+            </div>
           </div>
-          <div className="text">{komentar}</div>
         </div>
       </div>
+    );
+  }
+}
+
+export default function CommentList() {
+  const users = faker.helpers.multiple(generateRandomUser, { count: 10 });
+
+  return (
+    <div>
+      <Comments data={users}/>
     </div>
   );
 }
 
-function createRandomUser(){
+// function Comment({ name, img, registeredAt, comment }) {
+//   return (
+//     <div className="ui container comments">
+//       <div className="comment">
+//         <a href="/" className="avatar">
+//           <img alt="avatar" src={img} />
+//         </a>
+//         <div className="content">
+//           <a href="/" className="author">
+//             {name}
+//           </a>
+//           <div className="metadata">
+//             <span className="date">{time}</span>
+//           </div>
+//           <div className="text">{comment}</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+function generateRandomUser() {
   return {
     username: faker.internet.userName(),
     comment: faker.lorem.sentence(),
     avatar: faker.image.avatar(),
-    registeredAt: faker.date.past().toLocaleTimeString(),
+    time: faker.date.past().toLocaleDateString(),
+    like: Math.floor(Math.random()*100)
   };
 }
